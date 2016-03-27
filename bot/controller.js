389924576dcc0e0da170a606f3f6ec226b3cbff7
Michael Morrissey <mikehmorrissey@gmail.com>
2016-03-27 13:44:26 -0400

    test

--- bot/controller.js
+++ bot/controller.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv'
 // env variables
 dotenv.config()
 
-const storage = redisStorage({})
+const storage = redisStorage('redis://pub-redis-15683.us-east-1-3.4.ec2.garantiadata.com:15683')
 const controller = Botkit.slackbot({ storage })
 
 // apply middleware
@@ -17,5 +17,14 @@ controller.middleware.receive.use([receive, removeFormatting])
 controller.middleware.hears.use(hears)
 
 const beepboop = BeepBoop.start(controller, { debug: false })
+
+controller
+  .on('create_bot', (bot, config) => {
+    console.log(bot)
+  })
+
+controller.storage.users.all((err, teams) => {
+  console.log(teams)
+})
 
 export default controller
--- bot/middleware/removeFormatting.js
+++ bot/middleware/removeFormatting.js
@@ -1,32 +1,33 @@
 const removeFormatting = (bot, message, next) => {
-  let text = message.text
+  if (message.text) {
+    let text = message.text
 
-  text = text.replace(/<([@#!])?([^>|]+)(?:\|([^>]+))?>/, (m, type, link, label) => {
-    switch(type) {
-      case '@':
-        if (label) {
-          return label
-        } else {
-          return  `@${link}`
-        }
-      case '#':
-        if (label) {
-          return label
-        } else {
-          return `#${link}`
-        }
-      case '!':
-        return `@${link}`
-      default:
-        return link
-    }
-  })
+    text = text.replace(/<([@#!])?([^>|]+)(?:\|([^>]+))?>/, (m, type, link, label) => {
+      switch(type) {
+        case '@':
+          if (label) {
+            return label
+          } else {
+            return  `@${link}`
+          }
+        case '#':
+          if (label) {
+            return label
+          } else {
+            return `#${link}`
+          }
+        case '!':
+          return `@${link}`
+        default:
+          return link
+      }
+    })
 
-  message.text = text
-  console.log(message.text)
-
-  next()
-
+    message.text = text
+    next()
+  } else {
+    next()
+  }
 }
 
 export default removeFormatting
