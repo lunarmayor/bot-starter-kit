19574186781fbd937951f538b0eb0fc42252a8e9
Michael Morrissey <mikehmorrissey@gmail.com>
2016-04-03 18:26:17 -0400

    fix typo

--- bot/introduction.js
+++ bot/introduction.js
@@ -2,14 +2,12 @@ import controller from './controller'
 
 controller
   .on('create_bot', (bot, { createdBy }) => {
-    console.log(createdBy)
-    console.log(bot)
     bot.startPrivateConversation({ user: createdBy }, (err, convo) => {
       convo.say('Hey I\'m ollie!')
       convo.say('/invite me to a channel so I can get to work!')
     })
   })
-  .on('bot_channel_joined', (bot, message) => {
+  .on('bot_channel_join', (bot, message) => {
     bot.reply(message, 'Hi! Here to help!')
   })
 
