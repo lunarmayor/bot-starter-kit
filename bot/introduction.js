e030ed6db915e8d274cc927a00aee22ce40b4095
Michael Morrissey <mikehmorrissey@gmail.com>
2016-04-03 18:01:17 -0400

    debugging

--- bot/introduction.js
+++ bot/introduction.js
@@ -2,12 +2,14 @@ import controller from './controller'
 
 controller
   .on('create_bot', (bot, { createdBy }) => {
+    console.log(createdBy)
+    console.log(bot)
     bot.startPrivateConversation({ user: createdBy }, (err, convo) => {
       convo.say('Hey I\'m ollie!')
       convo.say('/invite me to a channel so I can get to work!')
     })
   })
-  .on('channel_joined', (bot, message) => {
+  .on('bot_channel_joined', (bot, message) => {
     bot.reply(message, 'Hi! Here to help!')
   })
 
