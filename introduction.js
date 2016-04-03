4e555f25bb4aac855a4a06525517c165b5a361db
Michael Morrissey <mikehmorrissey@gmail.com>
2016-04-03 17:48:33 -0400

    add introduction

---
+++ introduction.js
@@ -0,0 +1,13 @@
+import controller from './controller'
+
+controller
+  .on('create_bot', (bot, { createdBy }) => {
+    bot.startPrivateConversation({ user: createdBy }, (err, convo) => {
+      convo.say('Hey I\'m ollie!')
+      convo.say('/invite me to a channel so I can get to work!')
+    })
+  })
+  .on('channel_joined', (bot, message) => {
+    bot.reply(message, 'Hi! Here to help!')
+  })
+
